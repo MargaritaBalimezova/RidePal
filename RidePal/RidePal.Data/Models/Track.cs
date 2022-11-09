6b34318be01b9480117a43ef90e7ee61bc62cb04
Radoslav Berov <rado561.nbr@gmail.com>
2022-11-09 18:30:31 +0200

    Track and Trip Models Implemented

--- RidePal/RidePal.Data/Models/Track.cs
+++ RidePal/RidePal.Data/Models/Track.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using MovieForum.Data.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RidePal.Data.Models
 {
-    public class Track
+    public class Track: IHasId, IDeletable
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Rank { get; set; }
+        public int Duration { get; set; }
+        public Album Album { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+        public string PreviewURL { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
     }
 }
---
+++ RidePal/RidePal.Data/Models/Trip.cs
@@ -0,0 +1,17 @@
+﻿using MovieForum.Data.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RidePal.Data.Models
+{
+    internal class Trip : IHasId, IDeletable
+    {
+        public int Id { get; set; }
+        public string StartPoint { get; set; }
+        public string Destination { get; set; }
+        public int Duration { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}
